564fcb3dbc57161050f3d0d15e2354099a7e087e
snowism <73619383+snowism@users.noreply.github.com>
2022-08-26 23:17:48 +1200

    Delete js directory

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " mobilemenu";
-      
-    } else {
-      x.className = "topnav";
-    }
-  }
